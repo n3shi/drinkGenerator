9af7fb11d817ac7c96d156fff4ca79957f1126d6
n3shi <neshiteam@gmail.com>
2021-03-03 22:29:49 +0100

    swap attributes -> added Recipe attributes into Product class. upload basic Recipe class version

--- src/main/java/com/logic/drinkGenerator/Recipe.java
+++ src/main/java/com/logic/drinkGenerator/Recipe.java
@@ -1,5 +1,43 @@
 package com.logic.drinkGenerator;
 
-public class Recipe {
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "Recipes")
+public class Recipe {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long productId;
+		
+	private String description;
+	
+	private String author;
+	
+	private String taste;
+	
+	@OneToMany(mappedBy = "recipe")
+	private List<RecipesProducts> recipeProduct;
+
+	public Recipe(Long productId, String description, String author, String taste,
+			List<RecipesProducts> recipeProduct) {
+		super();
+		this.productId = productId;
+		this.description = description;
+		this.author = author;
+		this.taste = taste;
+		this.recipeProduct = recipeProduct;
+	}
 }
