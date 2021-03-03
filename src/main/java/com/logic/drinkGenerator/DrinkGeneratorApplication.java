ec93a6c4333d7d26ef9330ea750f6812dd574d5e
n3shi <neshiteam@gmail.com>
2021-03-03 21:45:19 +0100

    Update base project schema

---
+++ src/main/java/com/logic/drinkGenerator/DrinkGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.logic.drinkGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinkGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logic/drinkGenerator/DrinkGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logic.drinkGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinkGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
